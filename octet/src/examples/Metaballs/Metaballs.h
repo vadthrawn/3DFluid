3b349eab67ed6f980ec7aaa17c9f0df6b19eefbc
vadthrawn <vadthrawn@gmail.com>
2014-04-01 11:16:37 +0100

    Metaball Update
    Added color, velocities, and additional metaballs to the simulation
    Removed the ability to control one of the metaballs as they all move
    independantly now

--- octet/src/examples/Metaballs/Metaballs.h
+++ octet/src/examples/Metaballs/Metaballs.h
@@ -5,6 +5,8 @@ namespace octet
 		metaball_shader* mb_shader;
 		int* mb_positions;
 		float* pixels;
+		vec2* velocities;
+		float* colors;
 		int sizeOfPixels;
 		int numOfMetaballs;
 		vec4 mb_color;
@@ -19,6 +21,8 @@ namespace octet
 		{
 			delete[] mb_positions;
 			delete[] pixels;
+			delete[] velocities;
+			delete[] colors;
 			delete mb_shader;
 		}
 
@@ -54,17 +58,56 @@ namespace octet
 
 			glBufferData (GL_ARRAY_BUFFER, sizeOfPixels * sizeof (GLfloat), pixels, GL_STATIC_DRAW);
 
-			mb_threshold = 50;
+			mb_threshold = 30;
 			mb_color = vec4(1, 1, 1, 1);
 
-			numOfMetaballs = 3;
+			numOfMetaballs = 6;
 			mb_positions = new int[numOfMetaballs * 2];
+			velocities = new vec2[numOfMetaballs];
+			colors = new float[numOfMetaballs * 3];
+
 			mb_positions[0] = 200;
 			mb_positions[1] = 300;
+			velocities[0] = vec2 (2.0f, 3.0f);
+			colors[0] = 1.0f;
+			colors[1] = 0.0f;
+			colors[2] = 0.0f;
+
+
 			mb_positions[2] = 500;
 			mb_positions[3] = 600;
+			velocities[1] = vec2 (-1.0f, 2.5f);
+			colors[3] = 0.0f;
+			colors[4] = 1.0f;
+			colors[5] = 0.0f;
+
 			mb_positions[4] = 300;
 			mb_positions[5] = 400;
+			velocities[2] = vec2 (5.0f, -7.5f);
+			colors[6] = 0.0f;
+			colors[7] = 0.0f;
+			colors[8] = 1.0f;
+
+			mb_positions[6] = 600;
+			mb_positions[7] = 650;
+			velocities[3] = vec2 (-5.0f, 7.5f);
+			colors[9] = 1.0f;
+			colors[10] = 1.0f;
+			colors[11] = 0.0f;
+
+			mb_positions[8] = 50;
+			mb_positions[9] = 100;
+			velocities[4] = vec2 (2.5f, 5.0f);
+			colors[12] = 0.0f;
+			colors[13] = 1.0f;
+			colors[14] = 1.0f;
+
+			mb_positions[10] = 300;
+			mb_positions[11] = 400;
+			velocities[5] = vec2 (-3.0f, -1.0f);
+			colors[15] = 1.0f;
+			colors[16] = 1.0f;
+			colors[17] = 1.0f;
 			
 			modelToWorld.loadIdentity();
 			cameraToWorld.loadIdentity();
@@ -86,7 +129,7 @@ namespace octet
 
 			mat4t modelToProjection = mat4t::build_projection_matrix(modelToWorld, cameraToWorld, 0.5f, 256.0f);
 
-			mb_shader->render (modelToProjection, mb_color, mb_positions, mb_threshold, numOfMetaballs);
+			mb_shader->render (modelToProjection, colors, mb_positions, mb_threshold, numOfMetaballs);
 
 			glEnableVertexAttribArray (attribute_position);
 			glBindBuffer (GL_ARRAY_BUFFER, positionBuffer);
@@ -94,6 +137,10 @@ namespace octet
 			glVertexAttribPointer (attribute_position, 2, GL_FLOAT, GL_FALSE, 2 * sizeof (GLfloat), 0);
 			glDrawArrays (GL_POINTS, 0, sizeOfPixels);
 
+			for (int i = 0; i < numOfMetaballs; i++)
+				AdjustPosition (mb_positions[i * 2], mb_positions[i * 2 + 1], velocities[i]);
+
+			/*
 			if (is_key_down ('W'))
 				mb_positions[1] += 1;
 
@@ -105,6 +152,45 @@ namespace octet
 
 			if (is_key_down ('D'))
 				mb_positions[0] += 1;
+				*/
+		}
+
+		void CheckBoundries (int &positionX, int &positionY, vec2 &velocity)
+		{
+			int vx, vy;
+			get_viewport_size (vx, vy);
+
+			if (positionX < mb_threshold)
+			{
+				positionX = mb_threshold;
+				velocity.x() = - velocity.x();
+			}
+
+			else if (positionX > vx - mb_threshold)
+			{
+				positionX = vx - mb_threshold;
+				velocity.x() = -velocity.x();
+			}
+
+			if (positionY < mb_threshold)
+			{
+				positionY = mb_threshold;
+				velocity.y() = -velocity.y();
+			}
+
+			else if (positionY > vy - mb_threshold)
+			{
+				positionY = vy - mb_threshold;
+				velocity.y() = -velocity.y();
+			}
+		}
+
+		void AdjustPosition (int &positionX, int &positionY, vec2 &velocity)
+		{
+			positionX += (int)velocity.x();
+			positionY += (int)velocity.y();
+
+			CheckBoundries (positionX, positionY, velocity);
 		}
 	};
 }
